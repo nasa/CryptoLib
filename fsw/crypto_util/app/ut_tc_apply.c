43bf3fb0ed8a0796a108c396cc33c73139004396
Robert Brown <Robert.Brown@tmctechnologies.com>
2021-11-08 12:43:45 -0500

    Cleanup and Comments for UTs

--- fsw/crypto_util/app/ut_tc_apply.c
+++ fsw/crypto_util/app/ut_tc_apply.c
@@ -21,9 +21,25 @@
 #include "ut_tc_apply.h"
 #include "utest.h"
 
-// Initilization header?
+// TODO:  Should this be set up with a set of tests, or continue to Crypto_Init() each time.  For now I think the current setup is the best path.
 
-UTEST(HAPPY_PATH, TC_APPLY_SECURITY)
+// Inactive SA Database
+// TODO:  Should this return or continue to function as currently written when SA is not initalized?
+UTEST(TC_APPLY_SECURITY, NO_CRYPTO_INIT)
+{
+    // No Crypto_Init();
+    long buffer_size;
+    char *buffer = c_read_file("../../fsw/crypto_tests/data/raw_tc_sdls_ping.dat", &buffer_size);
+    uint32 buffer_size_i = (uint32) buffer_size;
+
+    uint8 *ptr_enc_frame = NULL;
+    uint32 enc_frame_len;
+
+    ASSERT_EQ(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len), 0);
+}
+
+// Nominal Test.  This should read a raw_tc_sdls_ping.dat file, continue down the "happy path", and return OS_SUCCESS
+UTEST(TC_APPLY_SECURITY, HAPPY_PATH)
 {
     //Setup & Initialize CryptoLib
     Crypto_Init();
@@ -34,10 +50,11 @@ UTEST(HAPPY_PATH, TC_APPLY_SECURITY)
     uint8 *ptr_enc_frame = NULL;
     uint32 enc_frame_len;
 
-    ASSERT_TRUE(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len) == 0);
+    ASSERT_EQ(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len), 0);
 }
 
-UTEST(BAD_SPACE_CRAFT_ID, TC_APPLY_SECURITY)
+// Bad Space Craft ID.  This should pass the flawed .dat file, and return OS_ERROR
+UTEST(TC_APPLY_SECURITY, BAD_SPACE_CRAFT_ID)
 {
     //Setup & Initialize CryptoLib
     Crypto_Init();
@@ -48,10 +65,12 @@ UTEST(BAD_SPACE_CRAFT_ID, TC_APPLY_SECURITY)
     uint8 *ptr_enc_frame = NULL;
     uint32 enc_frame_len;
 
-    ASSERT_FALSE(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len) == 0);
+    ASSERT_EQ(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len), -1);
 }
 
-UTEST(BAD_VIRTUAL_CHANNEL_ID, TC_APPLY_SECURITY)
+// TODO:  This does not report the correct error.  It returns the correctly, but complains of an incorrect SCID
+//        This should return OS_ERROR
+UTEST(TC_APPLY_SECURITY, BAD_VIRTUAL_CHANNEL_ID)
 {
     //Setup & Initialize CryptoLib
     Crypto_Init();
@@ -62,10 +81,14 @@ UTEST(BAD_VIRTUAL_CHANNEL_ID, TC_APPLY_SECURITY)
     uint8 *ptr_enc_frame = NULL;
     uint32 enc_frame_len;
 
-    ASSERT_FALSE(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len) == 0);
+    ASSERT_EQ(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len), -1);
 }
 
-UTEST(NULL_BUFFER, TC_APPLY_SECURITY)
+// This test should test how to handle a null buffer being passed into the ApplySecurity Function.
+// Currently this functionality isn't handled properly, and casues a seg-fault.
+// TODO:  We need to determine how this would return, as it will help in other test cases.
+//        Should this return the original buffer, a null pointer, OS_ERROR, etc?
+UTEST(TC_APPLY_SECURITY, NULL_BUFFER)
 {
     //Setup & Initialize CryptoLib
     Crypto_Init();
@@ -76,8 +99,27 @@ UTEST(NULL_BUFFER, TC_APPLY_SECURITY)
     uint8 *ptr_enc_frame = NULL;
     uint32 enc_frame_len;
 
-    ASSERT_FALSE(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len) == 0);
+    ASSERT_EQ(Crypto_TC_ApplySecurity(buffer, buffer_size_i, &ptr_enc_frame, &enc_frame_len), -1);
 }
+
+//TODO: 
+/*  What should be returned if something goes wrong with Control Command Flag?
+    Should a NULL pointer be returned....THe original pointer?
+    We need to decide on this functionality and write a test for this
+
+    We should probably have more error codes than OS_SUCCESS and OS_ERROR
+
+    Some way to modify and test the SA?
+
+    Authentication Tests
+        When Ready / Complete?
+
+    Encryption Tests
+        When Ready / Complete?
+    
+    Authenticated Encryption Tests
+        When Ready / Complete
+*/
 
 
 
