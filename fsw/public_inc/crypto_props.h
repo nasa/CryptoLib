15a061ef8218e4d282369bf12b32fdc575af985e
AryJpl <aryan.e.naim@jpl.nasa.gov>
2021-11-30 15:14:24 -0800

    logic: Simple key,value property management. 1) crypto_print.c was accidently delete so its added back in. 2) Headers & implementations files related to the branch config_engine were added in again with changes as per comments in the pull request whats added: crypto_props.h,crypto_props.c. IMPORTANT: Example usage is in the file ../unit_test/config_props/crypto_props_unit_tests.c. Sample properties file is under that path: ../unit_test/config_props/config_props.txt

---
+++ fsw/public_inc/crypto_props.h
@@ -0,0 +1,149 @@
+/*
+ This header represents functions required for property management via linked list
+ * data structure.  
+ */
+/* 
+ * File:   crypto_props.h
+ * Author: Ary Naim (aryan.e.naim@jpl.nasa.gov)
+ *
+ * Created on November 4, 2021, 9:15 AM
+ */
+
+#ifndef CRYPTO_PROPS_H
+#define CRYPTO_PROPS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stdlib.h>
+#define DEFAULT_MAX_KEY_SIZE_IN_BYTES       256
+#define DEFAULT_MAX_VALUE_SIZE_IN_BYTES     4096
+    
+/*represents a single linked list node with (key,value) pair*/
+typedef struct crypto_config_node
+{
+    char* key; 
+    char* value; 
+    struct crypto_config_node* p_next; 
+    
+} crypto_config_node;
+
+/*represents all properties*/
+typedef struct crypto_config_list
+{
+    crypto_config_node* p_head;
+    int KEY_SIZE_IN_BYTES; 
+    int VALUE_SIZE_IN_BYTES;  
+    
+} crypto_config_list;
+
+/*declare variables*/
+extern crypto_config_list* p_self;
+/*===========================================================================
+Function:           crypto_config_alloc      
+Description:        allocates heap memory for the wrapper crypto_config_list 
+ *                  that contains the head node.
+Inputs:             max_key_size in bytes. 
+ *                  Input of 0 or greater than 4096 will default to 256 bytes 
+ *                  length for the key size. 
+ * 
+ *                  max_value_size in bytes. 
+ *                  Input of 0 or greater than 4096 will default to 4096 bytes 
+ *                  length for the value size. 
+Outputs:            pointer to crypto_config_list
+References:
+Example call:       crypto_config_list* props = crypto_config_alloc(0,0)
+Note:               Passing in max_key_size=0 and/or max_value_size=0 will print 
+ *                  a warning just letting you know that defaults values of 
+ *                  max_key_size=256 and max_value_size=4096 were used instead. 
+==========================================================*/
+crypto_config_list* crypto_config_alloc(int max_key_size, int max_value_size);
+
+/*===========================================================================
+Function:           crypto_config_free
+Description:        Frees heap memory allocated to crypto_config_list & the
+ *                  linked list within crypto_config_list. 
+Inputs:             crypto_config_list** pp_self - pass by reference         
+Outputs:            void
+References: 
+Example call:       crypto_config_free(&props); 
+==========================================================*/
+void crypto_config_free(crypto_config_list** pp_self);
+
+/*===========================================================================
+Function:           crypto_config_props_add_property      
+Description:        adds a new key & value node to the end of the linkedlist.      
+Inputs:             char* key - some string. keys are case sensitive.
+ *                  Therefore key_a is different than key_A. 
+ *                  char* value- some string          
+Outputs:            1 - success 
+ *                  0 - fail       
+References: 
+Example call:       crypto_config_props_add_property("key_a","some value");
+Note:               since this is a linked list there is no efficient way to guard against
+                    adding the duplicate keys therefore if the key exists just update it using
+                    crypto_config_props_set_property_value_helper else add in a new key,value. 
+==========================================================*/
+int crypto_config_props_add_property(char* key, char* value);
+
+/*===========================================================================
+Function:           crypto_config_get_property_value      
+Description:        if a node exists with that key then the string value is 
+                    returned else "/0" string is returned.      
+Inputs:             char* key - some string key       
+Outputs:            string or "/0"       
+References:
+Example call:       crypto_config_get_property_value("key_a")
+==========================================================*/
+char* crypto_config_get_property_value(char* key); 
+
+/*===========================================================================
+Function:           crypto_config_set_property_value      
+Description:        If a node with the input key exists its value will be
+ *                  overwritten with input parameter char* value; 
+Inputs:             char* key- some string. keys are case sensitive.   
+ *                  Therefore key_a is different than key_A.        
+Outputs:            1 - success 
+ *                  0 - fail       
+References: 
+Example call:      crypto_config_set_property_value("key_a","AAA")
+==========================================================*/
+int crypto_config_set_property_value(char* key, char* value);
+
+/*===========================================================================
+Function:      
+Description:        This function will parse files with the format:
+                    #Comment#
+                    Key=value,
+                    key=value,
+                    OR:
+                    #Comment
+ *                  //Comment 
+                    Key=value,key=value,key=value
+/*Inputs:           char* file_path - path of the file on the filesystem
+Outputs:            1 or greater - success. Meaning read & added 1 or more data 
+ *                  lines successfully. 
+ *                  0 - fail  
+References:
+Example call:      crypto_config_load_properties("/<your_path>/docs/config_props.txt")
+==========================================================*/
+int crypto_config_load_properties(char* file_path);
+
+/*===========================================================================
+Function:           crypto_config_print_all_props                 
+Description:        Iterates through linkedlist and prints all key & values to 
+ *                  system out for debugging.   
+Inputs:             crypto_config_list* p_self         
+Outputs:            void        
+References: 
+Example call:       crypto_config_print_all_props(props)
+==========================================================*/
+void crypto_config_print_all_props(crypto_config_list* p_self);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CRYPTO_PROPS_H */
+
---
+++ fsw/src/crypto_props.c
@@ -0,0 +1,380 @@
+/*
+ This implementation file implements functions from crypto_props.h
+ * which are required for key,value property management via linked list
+ * data structure.  
+ */
+/* 
+ * File:   crypto_props.c
+ * Author: Ary Naim (aryan.e.naim@jpl.nasa.gov)
+ *
+ * Created on November 4, 2021, 9:37 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "crypto_props.h"
+
+crypto_config_list* p_self = NULL; 
+/*=============================================================================
+Private Declarations
+==============================================================================*/
+crypto_config_list* crypto_config_alloc_helper(int max_key_size, int max_value_size, crypto_config_list** props);
+int crypto_config_props_set_property_value_helper(char* key, char* value, crypto_config_list** props);
+char* crypto_config_props_get_property_value_helper(char* key, crypto_config_list** props);
+int crypto_config_props_add_property_helper(char* key, char* value,crypto_config_list** pp_self); 
+int crypto_config_props_remove_property_helper(char* key,crypto_config_list** pp_self); 
+int safe_copy(int destination_size,char* src);
+
+/*=============================================================================
+ * Implementations
+==============================================================================*/
+crypto_config_list* crypto_config_alloc(int max_key_size, int max_value_size) {
+   return crypto_config_alloc_helper(max_key_size, max_value_size,&p_self);
+}
+
+crypto_config_list* crypto_config_alloc_helper(int max_key_size, int max_value_size, crypto_config_list** props)
+{
+     if (NULL == p_self) {
+        p_self = (crypto_config_list*) calloc(1, sizeof(crypto_config_list));
+        //set max key size
+        if (max_key_size > 0 && max_key_size<=4096) {
+            (*props)->KEY_SIZE_IN_BYTES = max_key_size;
+        } else {
+              //defaults to 256 
+             (*props)->KEY_SIZE_IN_BYTES = DEFAULT_MAX_KEY_SIZE_IN_BYTES;
+             fprintf(stderr,"WARNING crypto_config,crypto_config_alloc_helper(), "
+             "max_key_size MUST be 0<max_key_size<=4096.max_key_size is set to "
+                     "default 256.\n");
+        }
+        //set max value size
+        if (max_value_size > 0 && max_value_size<=4096) {
+            (*props)->VALUE_SIZE_IN_BYTES  = max_value_size;
+        } else {
+             //defaults to 4096
+             (*props)->VALUE_SIZE_IN_BYTES  = DEFAULT_MAX_VALUE_SIZE_IN_BYTES;
+             fprintf(stderr,"WARNING crypto_config,crypto_config_alloc_helper(), "
+             "max_value_size MUST be 0<max_value_size<=4096.max_value_size is set to "
+                     "default 4096.\n");
+        }
+    }
+    return p_self;
+}
+
+void crypto_config_free(crypto_config_list** pp_self)
+{
+    if (NULL!=pp_self && NULL!=(*pp_self))
+    {
+        crypto_config_node* p_current = (*pp_self)->p_head; 
+        crypto_config_node* p_next = NULL; 
+        //iterate and free the nodes 
+        while (NULL!=p_current)
+        {
+            p_next = p_current->p_next;
+            //free child attributes 
+            free(p_current->key);
+            free(p_current->value);
+            //free the node 
+            free(p_current);
+            p_current = p_next; 
+           
+        }
+        //free the super structure 
+        free (*pp_self);
+        *pp_self = NULL; 
+    }
+}
+
+int crypto_config_props_add_property(char* key, char* value)
+{
+    int status = false; 
+    //Note: since this is a linked list there is no efficient way to guard against
+    //adding duplicate keys therefore if the key exists just update it using
+    //crypto_config_props_set_property_value_helper else add in a new key,value.
+    status = crypto_config_props_set_property_value_helper(key,value,&p_self);
+    if (status)
+    {
+        return status; 
+    }
+    else
+    {
+        return crypto_config_props_add_property_helper(key,value,&p_self);
+    }
+}
+
+int crypto_config_props_add_property_helper(char* key, char* value, crypto_config_list** pp_self) {
+    int status = false;
+    if (NULL != key && strlen(key) > 0 && NULL != value && NULL != pp_self && NULL != (*pp_self)) {
+
+        //is the head NULL?  
+        if (NULL == (*pp_self)->p_head) {
+
+
+            if (safe_copy((*pp_self)->KEY_SIZE_IN_BYTES, key)
+                    && safe_copy((*pp_self)->VALUE_SIZE_IN_BYTES, value)) {
+                //if head is NULL then allocate memory for it 
+                (*pp_self)->p_head = calloc(1, sizeof (crypto_config_node));
+                (*pp_self)->p_head->key = calloc((*pp_self)->KEY_SIZE_IN_BYTES, sizeof (char));
+                (*pp_self)->p_head->value = calloc((*pp_self)->VALUE_SIZE_IN_BYTES, sizeof (char));
+                //set key & value
+                strcpy((*pp_self)->p_head->key, key);
+                strcpy((*pp_self)->p_head->value, value);
+                status = true;
+            } else {
+                fprintf(stderr,"ERROR crypto_config,crypto_config_props_add_property_helper(), "
+                        "prevented unsafe copy.\n");
+                fprintf(stderr,"source key size=%lu, destination key size=%d,source value size=%lu, "
+                        "destination value size=%d.\n",
+                        strlen(key),
+                        (*pp_self)->KEY_SIZE_IN_BYTES,
+                        strlen(value),
+                        (*pp_self)->VALUE_SIZE_IN_BYTES);
+            }
+        }            //head is NOT NULL 
+        else {
+            crypto_config_node* p_current = (*pp_self)->p_head;
+            crypto_config_node* p_previous = NULL;
+            while (NULL != p_current) {
+                p_previous = p_current;
+                p_current = p_current->p_next;
+
+            }//end while 
+
+            //set key & value
+            if (safe_copy((*pp_self)->KEY_SIZE_IN_BYTES, key)
+                    && safe_copy((*pp_self)->VALUE_SIZE_IN_BYTES, value)) {
+                //add a new node to the end 
+                p_previous->p_next = calloc(1, sizeof (crypto_config_node));
+                p_previous->p_next->key = calloc((*pp_self)->KEY_SIZE_IN_BYTES, sizeof (char));
+                p_previous->p_next->value = calloc((*pp_self)->VALUE_SIZE_IN_BYTES, sizeof (char));
+                //set values 
+                strcpy(p_previous->p_next->key, key);
+                strcpy(p_previous->p_next->value, value);
+                status = true;
+            } else {
+                fprintf(stderr,"ERROR crypto_config,crypto_"
+                        "config_props_add_property_helper(), prevented unsafe "
+                        "copy.\n");
+                fprintf(stderr,"source key size=%lu, destination key size=%d,source value size=%lu, "
+                        "destination value size=%d.\n", strlen(key),
+                        (*pp_self)->KEY_SIZE_IN_BYTES,
+                        strlen(value),
+                        (*pp_self)->VALUE_SIZE_IN_BYTES);
+            }
+
+        }
+    } else {
+        fprintf(stderr,"ERROR, crypto_config_props_add_property_helper() inputs are empty or null.\n");
+    }
+
+    return status;
+}
+
+int crypto_config_set_property_value(char* key, char* value)
+{
+    return crypto_config_props_set_property_value_helper(key,value,&p_self);
+}
+
+int crypto_config_props_set_property_value_helper(char* key, char* value, crypto_config_list** pp_self)
+{
+    int status = false; 
+    //iterate through the link list until you reach the right node
+    if (NULL!=key && strlen(key)> 0 && NULL!=value && NULL!=pp_self && NULL!=(*pp_self))
+    {
+        crypto_config_node* p_current = (*pp_self)->p_head; 
+        while (p_current!=NULL)
+        {
+            //returns 0 if NOT equal 
+            if (strcmp(p_current->key,key)!=0)
+            {
+                p_current= p_current->p_next; 
+            }
+            else
+            {
+                if (safe_copy((*pp_self)->KEY_SIZE_IN_BYTES,value))
+                {   
+                    //reset old values to empty 
+                    memset(p_current->value, 0,(*pp_self)->VALUE_SIZE_IN_BYTES);
+                    //replace old value with the new 
+                    strcpy(p_current->value,value);  
+                    status = true; 
+                }
+                else
+                {
+                    //TODO: print error 
+                    fprintf(stderr,"ERROR crypto_config_props_set_property_value_helper(),"
+                            "unsafe copy source value size=%lu is larger than destination "
+                            "size=%lu.\n",strlen(value),strlen(p_current->value));
+                }
+                break; 
+                
+            }
+            
+        }//end while 
+    }
+    else
+    {
+        fprintf(stderr,"ERROR, crypto_config_props_set_property_value_helper() inputs are empty or null.\n");
+    }
+    return status; 
+}
+
+char* crypto_config_get_property_value(char* key)
+{
+    return crypto_config_props_get_property_value_helper(key,&p_self);
+}
+
+char* crypto_config_props_get_property_value_helper(char* key, crypto_config_list** pp_self)
+{
+    //iterate through the link list until you reach the right node
+    if (NULL!=key && strlen(key)> 0 && NULL!=(*pp_self))
+    {
+        crypto_config_node* p_current = (*pp_self)->p_head; 
+        while (p_current!=NULL)
+        {
+            //returns 0 if NOT equal 
+            if (strcmp(p_current->key,key)!=0)
+            {
+                p_current= p_current->p_next; 
+            }
+            else
+            {
+                return p_current->value; 
+            }
+        }//end while 
+    }
+    else
+    {
+        fprintf(stderr,"ERROR, crypto_config_props_get_property_value_helper() inputs are empty or null.\n");
+    }
+    return "\0"; 
+}
+
+int crypto_config_load_properties(char* file_path)
+{
+    int status = false; 
+    if (NULL!=file_path)
+    {
+        //open file
+        FILE* p_file=NULL;
+        p_file=fopen(file_path,"r");
+        //counter to keep track of the number of parsed key=values that were added successfully 
+        int read_added_props_counter = 0; 
+        if (NULL!=p_file)
+        {
+            char key[p_self->KEY_SIZE_IN_BYTES];
+            char val[p_self->VALUE_SIZE_IN_BYTES];
+            char current_line[p_self->VALUE_SIZE_IN_BYTES]; 
+            const char* line_delimiter = "="; 
+            int index =-1;
+            int add_status = false; 
+            //while loop to parse the file 
+            while(fgets(current_line, p_self->VALUE_SIZE_IN_BYTES-1,p_file))
+            {
+                // Remove trailing newline
+                current_line[strcspn(current_line, "\n")] = 0;
+                //split the line based on the index of the delimeter "="
+                index=strcspn(current_line,line_delimiter);
+                if (index>=0)
+                {
+                    //validate this line valid key=value data line rather than comment or empty line
+                    if (strcmp(current_line,"\n") !=0 /*not empty line*/
+                            && strcmp(current_line,"\r\n")!=0 /*not empty line*/
+                            && strlen(current_line)>=2 /*length must be >=2*/
+                            && current_line[0]!='#' /*1st char cannot be #*/
+                            &&  (current_line[1]!='/' && current_line[0]!='/') /*1st & 2nd char cannot be '/' */)
+                    {
+                        //printf("DEBUG current_line DATA=%s\n",current_line);
+                        //1) pass the key 
+                        int i=0; 
+                        while (i<index)
+                        {
+                            key[i]=current_line[i];
+                            i++; 
+                        }
+                        key[i] = '\0';
+                        //2) parse the value 
+                        i=index+1;
+                        int j=0;
+                        while (current_line[i]!='\0' && j<p_self->VALUE_SIZE_IN_BYTES)
+                        {
+                            val[j]=current_line[i];
+                            i++;
+                            j++;
+                        }
+                        val[j] = '\0'; 
+                        //printf("DEBUG key=%s,value=%s\n",key,val);
+                        //3) add the key,value to the properties linkedlist
+                        add_status = crypto_config_props_add_property(key,val);
+                        if (add_status>0)
+                        {
+                            read_added_props_counter = read_added_props_counter + 1; 
+                        }
+                        
+                    }
+                    else
+                    {   //delimiter was detected but this starts with a comment character therefore it will NOT be parsed
+                        //printf("COMMENT=%s\n",current_line);
+                    }
+                }
+                else
+                {
+                    //do nothing there was no "=" delimiter detected 
+                }
+                
+            }//end while loop thats parses the file 
+            status = read_added_props_counter; 
+            if (NULL!=p_file)
+            {
+                 fclose(p_file);
+            }
+        }//end if file pointer is not NULL
+        else
+        {
+            fprintf(stderr,"ERROR, crypto_config_load_properties() error opening file!.\n");
+        }
+
+    }//end if file path is not NULL
+    else
+    {
+        fprintf(stderr,"ERROR, crypto_config_load_properties() file path is NULL.\n");
+    }
+    return status; 
+}
+
+void crypto_config_print_all_props(crypto_config_list* p_self)
+{
+    if (NULL!=p_self)
+    {
+         crypto_config_node* p_current = p_self->p_head;
+         while (NULL!=p_current)
+         {
+             printf("key=%s,value=%s\n",p_current->key,p_current->value);
+             p_current = p_current->p_next; 
+             
+         }
+    }
+}
+/*===========================================================================
+Function:           safe_copy
+Description:        safe_copy is a helper function to guard against unsafe copies.
+ *                  strlcp() is not available in all compilers, therefore this 
+                    function returns true(1) if destination size >= src.   
+Inputs:             dest - string 
+ *                  src - string                   
+Outputs:            1 or 0 
+References:
+Note:               since at the time of copy memory has not been allocated for the destination buffer we
+ *                  we have to use expected destination size as set by alloc() in comparison to src length. 
+==========================================================*/
+int safe_copy(int destination_size,char* src) {
+    int flag = false;
+    if (NULL!= src) {
+       if (destination_size >= strlen(src)) {
+            flag = true;
+        }
+    }
+    return flag;
+}
+
