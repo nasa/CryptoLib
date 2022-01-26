c2bb6da0a7474d2a8403e15fcd5a3b5950e454ac
Ibraheem Saleh <ibraheem.y.saleh@jpl.nasa.gov>
2022-01-26 14:16:02 -0800

    Remove invalid tests & unit test config -- AES-GCM must use tags

--- src/crypto_sadb/sadb_mariadb_admin_scripts/create_sadb_jpl_unit_test_security_associations.sql
+++ src/crypto_sadb/sadb_mariadb_admin_scripts/create_sadb_jpl_unit_test_security_associations.sql
@@ -9,7 +9,7 @@ VALUES (1,'kmc/test/key130',3,X'01',1,1,12,16,X'000000000000000000000001',19,X'0
 INSERT INTO security_associations (spi,ekid,sa_state,ecs,est,ast,shivf_len,stmacf_len,iv,abm_len,abm,arcw_len,arcw,arc_len,tfvn,scid,vcid,mapid)
 VALUES (2,'kmc/test/key130',3,X'01',1,1,12,16,X'000000000000000000000001',19,X'00000000000000000000000000000000000000',1,5,0,0,44,1,0);
 
--- SA 3 - OPERATIONAL; ENC Only - ARCW:5; AES-GCM; IV:00...01; IV-len:12; MAC-len:16; Key-ID: 130, SCID 44, VC-2
+-- SA 3 - OPERATIONAL; ENC Only (invalid config, AEAD algorithms must have a tag/mac) - ARCW:5; AES-GCM; IV:00...01; IV-len:12; MAC-len:16; Key-ID: 130, SCID 44, VC-2
 INSERT INTO security_associations (spi,ekid,sa_state,ecs,est,ast,shivf_len,stmacf_len,iv,abm_len,abm,arcw_len,arcw,arc_len,tfvn,scid,vcid,mapid)
 VALUES (3,'kmc/test/key130',3,X'01',1,0,12,0,X'000000000000000000000001',19,X'00000000000000000000000000000000000000',1,5,0,0,44,2,0);
 
--- util/src_util/ut_kmc_crypto.c
+++ util/src_util/ut_kmc_crypto.c
@@ -78,56 +78,57 @@ UTEST(KMC_CRYPTO, HAPPY_PATH_APPLY_SEC_ENC_AND_AUTH)
     free(ptr_enc_frame);
     ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
 }
-/**
- * @brief Unit Test: Nominal Encryption with KMC Crypto Service && JPL Unit Test MariaDB
- **/
-UTEST(KMC_CRYPTO, HAPPY_PATH_APPLY_SEC_ENC_ONLY)
-{
-    // Setup & Initialize CryptoLib
-    Crypto_Config_CryptoLib(SADB_TYPE_MARIADB, CRYPTOGRAPHY_TYPE_KMCCRYPTO, CRYPTO_TC_CREATE_FECF_TRUE, TC_PROCESS_SDLS_PDUS_FALSE, TC_NO_PUS_HDR,
-                            TC_IGNORE_SA_STATE_FALSE, TC_IGNORE_ANTI_REPLAY_TRUE, TC_UNIQUE_SA_PER_MAP_ID_FALSE,
-                            TC_CHECK_FECF_TRUE, 0x3F);
-    Crypto_Config_MariaDB("sadb_user", "sadb_password", "localhost","sadb", 3306, CRYPTO_FALSE, NULL, NULL, NULL, NULL);
-    Crypto_Config_Kmc_Crypto_Service("https", "asec-cmdenc-srv1.jpl.nasa.gov", 8443, "crypto-service", "/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/local-test-cert.pem", "PEM","/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/local-test-key.pem",NULL,"/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/ammos-ca-bundle.crt", NULL, NULL, CRYPTO_FALSE);
-    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 0, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
-    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 1, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
-    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 2, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
-    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 3, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
-    int32_t status = Crypto_Init();
-
-    char *raw_tc_jpl_mmt_scid44_vcid1= "202c0808000001361c";
-    char *raw_tc_jpl_mmt_scid44_vcid1_expect = NULL;
-    int raw_tc_jpl_mmt_scid44_vcid1_expect_len = 0;
-
-    hex_conversion(raw_tc_jpl_mmt_scid44_vcid1, &raw_tc_jpl_mmt_scid44_vcid1_expect, &raw_tc_jpl_mmt_scid44_vcid1_expect_len);
-
-    uint8_t *ptr_enc_frame = NULL;
-    uint16_t enc_frame_len = 0;
-
-    ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
-
-    printf("Frame before encryption:\n");
-    for (int i=0; i<raw_tc_jpl_mmt_scid44_vcid1_expect_len; i++)
-    {
-        printf("%02x ", (uint8_t)raw_tc_jpl_mmt_scid44_vcid1_expect[i]);
-    }
-    printf("\n");
-
-    status = Crypto_TC_ApplySecurity((uint8_t *)raw_tc_jpl_mmt_scid44_vcid1_expect, raw_tc_jpl_mmt_scid44_vcid1_expect_len, &ptr_enc_frame, &enc_frame_len);
-    ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
-    printf("Frame after encryption:\n");
-    for (int i=0; i<enc_frame_len; i++)
-    {
-        printf("%02x ", ptr_enc_frame[i]);
-    }
-    printf("\n");
-
-
-    Crypto_Shutdown();
-    free(raw_tc_jpl_mmt_scid44_vcid1_expect);
-    free(ptr_enc_frame);
-    ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
-}
+//// Commenting out test - AEAD algorithms must have a tag -- Enc only config is invalid
+///**
+// * @brief Unit Test: Nominal Encryption with KMC Crypto Service && JPL Unit Test MariaDB
+// **/
+//UTEST(KMC_CRYPTO, HAPPY_PATH_APPLY_SEC_ENC_ONLY)
+//{
+//    // Setup & Initialize CryptoLib
+//    Crypto_Config_CryptoLib(SADB_TYPE_MARIADB, CRYPTOGRAPHY_TYPE_KMCCRYPTO, CRYPTO_TC_CREATE_FECF_TRUE, TC_PROCESS_SDLS_PDUS_FALSE, TC_NO_PUS_HDR,
+//                            TC_IGNORE_SA_STATE_FALSE, TC_IGNORE_ANTI_REPLAY_TRUE, TC_UNIQUE_SA_PER_MAP_ID_FALSE,
+//                            TC_CHECK_FECF_TRUE, 0x3F);
+//    Crypto_Config_MariaDB("sadb_user", "sadb_password", "localhost","sadb", 3306, CRYPTO_FALSE, NULL, NULL, NULL, NULL);
+//    Crypto_Config_Kmc_Crypto_Service("https", "asec-cmdenc-srv1.jpl.nasa.gov", 8443, "crypto-service", "/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/local-test-cert.pem", "PEM","/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/local-test-key.pem",NULL,"/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/ammos-ca-bundle.crt", NULL, NULL, CRYPTO_FALSE);
+//    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 0, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
+//    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 1, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
+//    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 2, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
+//    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 3, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
+//    int32_t status = Crypto_Init();
+//
+//    char *raw_tc_jpl_mmt_scid44_vcid1= "202c0808000001361c";
+//    char *raw_tc_jpl_mmt_scid44_vcid1_expect = NULL;
+//    int raw_tc_jpl_mmt_scid44_vcid1_expect_len = 0;
+//
+//    hex_conversion(raw_tc_jpl_mmt_scid44_vcid1, &raw_tc_jpl_mmt_scid44_vcid1_expect, &raw_tc_jpl_mmt_scid44_vcid1_expect_len);
+//
+//    uint8_t *ptr_enc_frame = NULL;
+//    uint16_t enc_frame_len = 0;
+//
+//    ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
+//
+//    printf("Frame before encryption:\n");
+//    for (int i=0; i<raw_tc_jpl_mmt_scid44_vcid1_expect_len; i++)
+//    {
+//        printf("%02x ", (uint8_t)raw_tc_jpl_mmt_scid44_vcid1_expect[i]);
+//    }
+//    printf("\n");
+//
+//    status = Crypto_TC_ApplySecurity((uint8_t *)raw_tc_jpl_mmt_scid44_vcid1_expect, raw_tc_jpl_mmt_scid44_vcid1_expect_len, &ptr_enc_frame, &enc_frame_len);
+//    ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
+//    printf("Frame after encryption:\n");
+//    for (int i=0; i<enc_frame_len; i++)
+//    {
+//        printf("%02x ", ptr_enc_frame[i]);
+//    }
+//    printf("\n");
+//
+//
+//    Crypto_Shutdown();
+//    free(raw_tc_jpl_mmt_scid44_vcid1_expect);
+//    free(ptr_enc_frame);
+//    ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
+//}
 /**
  * @brief Unit Test: Nominal Encryption with KMC Crypto Service && JPL Unit Test MariaDB
  **/
@@ -238,65 +239,66 @@ UTEST(KMC_CRYPTO, HAPPY_PATH_PROCESS_SEC_ENC_AND_AUTH)
     free(ptr_enc_frame);
     ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
 }
-/**
- * @brief Unit Test: Nominal Encryption with KMC Crypto Service && JPL Unit Test MariaDB
- **/
-UTEST(KMC_CRYPTO, HAPPY_PATH_PROCESS_SEC_ENC_ONLY)
-{
-    // Setup & Initialize CryptoLib
-    Crypto_Config_CryptoLib(SADB_TYPE_MARIADB, CRYPTOGRAPHY_TYPE_KMCCRYPTO, CRYPTO_TC_CREATE_FECF_TRUE, TC_PROCESS_SDLS_PDUS_FALSE, TC_NO_PUS_HDR,
-                            TC_IGNORE_SA_STATE_FALSE, TC_IGNORE_ANTI_REPLAY_TRUE, TC_UNIQUE_SA_PER_MAP_ID_FALSE,
-                            TC_CHECK_FECF_TRUE, 0x3F);
-    Crypto_Config_MariaDB("sadb_user", "sadb_password", "localhost","sadb", 3306, CRYPTO_FALSE, NULL, NULL, NULL, NULL);
-    Crypto_Config_Kmc_Crypto_Service("https", "asec-cmdenc-srv1.jpl.nasa.gov", 8443, "crypto-service", "/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/local-test-cert.pem", "PEM","/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/local-test-key.pem",NULL,"/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/ammos-ca-bundle.crt", NULL, NULL, CRYPTO_FALSE);
-    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 0, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
-    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 1, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
-    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 2, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
-    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 3, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
-    int32_t status = Crypto_Init();
-
-    char *enc_tc_jpl_mmt_scid44_vcid1= "202C0816000003000000000000000000000001669CD238";
-    char *enc_tc_jpl_mmt_scid44_vcid1_expect = NULL;
-    int enc_tc_jpl_mmt_scid44_vcid1_expect_len = 0;
-
-    // IV = 000000000000000000000001
-
-    TC_t *tc_processed_frame;
-    tc_processed_frame = malloc(sizeof(uint8_t) * TC_SIZE);
-
-    hex_conversion(enc_tc_jpl_mmt_scid44_vcid1, &enc_tc_jpl_mmt_scid44_vcid1_expect, &enc_tc_jpl_mmt_scid44_vcid1_expect_len);
-
-    uint8_t *ptr_enc_frame = NULL;
-
-    ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
-
-    printf("Encrypted Frame Before Processing:\n");
-    for (int i=0; i<enc_tc_jpl_mmt_scid44_vcid1_expect_len; i++)
-    {
-        printf("%02x ", (uint8_t)enc_tc_jpl_mmt_scid44_vcid1_expect[i]);
-    }
-    printf("\n");
-
-    status = Crypto_TC_ProcessSecurity((uint8_t *)enc_tc_jpl_mmt_scid44_vcid1_expect, &enc_tc_jpl_mmt_scid44_vcid1_expect_len, tc_processed_frame);
-    // ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
-    // Expected to fail -- KMC Crypto Service doesn't support AES/GCM with no AAD/MAC
-    ASSERT_EQ(CRYPTOGRAHPY_KMC_CRYPTO_SERVICE_GENERIC_FAILURE, status);
-    printf("Processed PDU:\n");
-    // for (int i=0; i<tc_processed_frame->tc_pdu_len; i++)
-    for (int i=0; i<2; i++)
-    {
-        printf("%02x ", tc_processed_frame->tc_pdu[i]);
-    }
-    printf("\n");
-
-    // ASSERT_EQ(0x00,tc_processed_frame->tc_pdu[0]);
-    // ASSERT_EQ( 0x01,tc_processed_frame->tc_pdu[1]);
-
-    Crypto_Shutdown();
-    free(enc_tc_jpl_mmt_scid44_vcid1_expect);
-    free(ptr_enc_frame);
-    // ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
-}
+//// Commenting out test - AEAD algorithms must have a tag -- Enc only config is invalid
+///**
+// * @brief Unit Test: Nominal Encryption with KMC Crypto Service && JPL Unit Test MariaDB
+// **/
+//UTEST(KMC_CRYPTO, HAPPY_PATH_PROCESS_SEC_ENC_ONLY)
+//{
+//    // Setup & Initialize CryptoLib
+//    Crypto_Config_CryptoLib(SADB_TYPE_MARIADB, CRYPTOGRAPHY_TYPE_KMCCRYPTO, CRYPTO_TC_CREATE_FECF_TRUE, TC_PROCESS_SDLS_PDUS_FALSE, TC_NO_PUS_HDR,
+//                            TC_IGNORE_SA_STATE_FALSE, TC_IGNORE_ANTI_REPLAY_TRUE, TC_UNIQUE_SA_PER_MAP_ID_FALSE,
+//                            TC_CHECK_FECF_TRUE, 0x3F);
+//    Crypto_Config_MariaDB("sadb_user", "sadb_password", "localhost","sadb", 3306, CRYPTO_FALSE, NULL, NULL, NULL, NULL);
+//    Crypto_Config_Kmc_Crypto_Service("https", "asec-cmdenc-srv1.jpl.nasa.gov", 8443, "crypto-service", "/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/local-test-cert.pem", "PEM","/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/local-test-key.pem",NULL,"/home/isaleh/git/KMC/CryptoLib-IbraheemYSaleh/util/etc/ammos-ca-bundle.crt", NULL, NULL, CRYPTO_FALSE);
+//    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 0, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
+//    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 1, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
+//    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 2, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
+//    Crypto_Config_Add_Gvcid_Managed_Parameter(0, 0x002C, 3, TC_HAS_FECF, TC_NO_SEGMENT_HDRS);
+//    int32_t status = Crypto_Init();
+//
+//    char *enc_tc_jpl_mmt_scid44_vcid1= "202C0816000003000000000000000000000001669CD238";
+//    char *enc_tc_jpl_mmt_scid44_vcid1_expect = NULL;
+//    int enc_tc_jpl_mmt_scid44_vcid1_expect_len = 0;
+//
+//    // IV = 000000000000000000000001
+//
+//    TC_t *tc_processed_frame;
+//    tc_processed_frame = malloc(sizeof(uint8_t) * TC_SIZE);
+//
+//    hex_conversion(enc_tc_jpl_mmt_scid44_vcid1, &enc_tc_jpl_mmt_scid44_vcid1_expect, &enc_tc_jpl_mmt_scid44_vcid1_expect_len);
+//
+//    uint8_t *ptr_enc_frame = NULL;
+//
+//    ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
+//
+//    printf("Encrypted Frame Before Processing:\n");
+//    for (int i=0; i<enc_tc_jpl_mmt_scid44_vcid1_expect_len; i++)
+//    {
+//        printf("%02x ", (uint8_t)enc_tc_jpl_mmt_scid44_vcid1_expect[i]);
+//    }
+//    printf("\n");
+//
+//    status = Crypto_TC_ProcessSecurity((uint8_t *)enc_tc_jpl_mmt_scid44_vcid1_expect, &enc_tc_jpl_mmt_scid44_vcid1_expect_len, tc_processed_frame);
+//    // ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
+//    // Expected to fail -- KMC Crypto Service doesn't support AES/GCM with no AAD/MAC
+//    ASSERT_EQ(CRYPTOGRAHPY_KMC_CRYPTO_SERVICE_GENERIC_FAILURE, status);
+//    printf("Processed PDU:\n");
+//    // for (int i=0; i<tc_processed_frame->tc_pdu_len; i++)
+//    for (int i=0; i<2; i++)
+//    {
+//        printf("%02x ", tc_processed_frame->tc_pdu[i]);
+//    }
+//    printf("\n");
+//
+//    // ASSERT_EQ(0x00,tc_processed_frame->tc_pdu[0]);
+//    // ASSERT_EQ( 0x01,tc_processed_frame->tc_pdu[1]);
+//
+//    Crypto_Shutdown();
+//    free(enc_tc_jpl_mmt_scid44_vcid1_expect);
+//    free(ptr_enc_frame);
+//    // ASSERT_EQ(CRYPTO_LIB_SUCCESS, status);
+//}
 /**
  * @brief Unit Test: Nominal Encryption with KMC Crypto Service && JPL Unit Test MariaDB
  * This doesn't work -- Apply Security Auth Only doesn't return the proper tag.
