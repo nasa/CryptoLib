4b1da5cbf82b40d141eaa5c29416ba08e53327ea
Robert Brown <Robert.Brown@tmctechnologies.com>
2022-08-11 14:16:18 -0400

    Added Auth Only Test

--- util/src_util/ut_tc_apply.c
+++ util/src_util/ut_tc_apply.c
@@ -139,6 +139,42 @@ UTEST(TC_APPLY_SECURITY, HAPPY_PATH_ENC)
 }
 
 /**
+ * @brief Unit Test: Nominal Encryption
+ **/
+UTEST(TC_APPLY_SECURITY, HAPPY_PATH_AUTH)
+{
+    // Setup & Initialize CryptoLib
+    Crypto_Init_Unit_Test();
+    char* raw_tc_sdls_ping_h = "20030015000080d2c70008197f0b00310000b1fe3128";
+    char* raw_tc_sdls_ping_b = NULL;
+    int raw_tc_sdls_ping_len = 0;
+    SadbRoutine sadb_routine = get_sadb_routine_inmemory();
+
+    hex_conversion(raw_tc_sdls_ping_h, &raw_tc_sdls_ping_b, &raw_tc_sdls_ping_len);
+
+    uint8_t* ptr_enc_frame = NULL;
+    uint16_t enc_frame_len = 0;
+
+    int32_t return_val = CRYPTO_LIB_ERROR;
+
+    SecurityAssociation_t* test_association = malloc(sizeof(SecurityAssociation_t) * sizeof(uint8_t));
+    // Expose the SADB Security Association for test edits.
+    sadb_routine->sadb_get_sa_from_spi(1, &test_association);
+    test_association->sa_state = SA_NONE;
+    sadb_routine->sadb_get_sa_from_spi(4, &test_association);
+    test_association->sa_state = SA_OPERATIONAL;
+    test_association->est = 0;
+    test_association->arsn_len = 0;
+
+    return_val =
+        Crypto_TC_ApplySecurity((uint8_t* )raw_tc_sdls_ping_b, raw_tc_sdls_ping_len, &ptr_enc_frame, &enc_frame_len);
+    Crypto_Shutdown();
+    free(raw_tc_sdls_ping_b);
+    free(ptr_enc_frame);
+    ASSERT_EQ(CRYPTO_LIB_SUCCESS, return_val);
+}
+
+/**
  * @brief Unit Test: Nominal Authorized Encryption
  **/
 UTEST(TC_APPLY_SECURITY, HAPPY_PATH_AUTH_ENC)
