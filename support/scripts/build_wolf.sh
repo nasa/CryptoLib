58d0445af992d44a7d953f4e133123e5951958e2
Robert Brown <91291114+rjbrown2@users.noreply.github.com>
2023-10-03 17:10:36 -0400

    Update build_wolf.sh

--- support/scripts/build_wolf.sh
+++ support/scripts/build_wolf.sh
@@ -9,4 +9,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/env.sh
 
-cmake $BASE_DIR -DCODECOV=1 -DDEBUG=1 -DCRYPTO_LIBGCRYPT=0 -DCRYPTO_WOLFSSL=1 -DSUPPORT=1 -DTEST=1 -DTEST_ENC=1 && make && make test
+cmake $BASE_DIR -DCODECOV=1 -DDEBUG=1 -DCRYPTO_LIBGCRYPT=0 -DCRYPTO_WOLFSSL=1 -DTEST=1 -DTEST_ENC=1 && make && make test
