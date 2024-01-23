ae01a58a74e7b8f07afa1aed4b2c56cacf13a7a6
D. Cody Cutright <David.C.Cutright@nasa.gov>
2024-01-23 08:45:01 -0500

    Add flag to convenience script

--- support/scripts/build_kmc.sh
+++ support/scripts/build_kmc.sh
@@ -9,4 +9,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/env.sh
 
-cmake $BASE_DIR -DCODECOV=1 -DDEBUG=1 -DCRYPTO_KMC=1 -DKEY_KMC=1 -DMC_DISABLED=1 -DSA_MARIADB=1 -DTEST=1 -DTEST_ENC=1 && make && make test
+cmake $BASE_DIR -DCODECOV=1 -DDEBUG=1 -DCRYPTO_KMC=1 -DKEY_KMC=1 -DMC_DISABLED=1 -DSA_MARIADB=1 -DTEST=1 -DTEST_ENC=1 -DKMC_CFFI_EXCLUDE && make && make test
