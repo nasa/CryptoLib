d22ec9f21dcf5682881dd288e19bb7623197abc4
Robert Brown <91291114+rjbrown2@users.noreply.github.com>
2024-02-20 17:36:10 +0000

    [#206] Add RHEL Convenience Script

---
+++ support/scripts/build_rhel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -i
+#
+# Convenience script for CryptoLib development
+# Will build in current directory
+#
+#  ./build_rhel.sh
+#
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/env.sh
+
+cmake $BASE_DIR -DCODECOV=1 -DDEBUG=1 -DMC_INTERNAL=1 -DTEST=1 -DTEST_ENC=1 && make && make test
