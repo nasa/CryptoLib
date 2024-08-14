55b06ee3d7a442bbae9cd9aa5640d1ebaeff2214
Lucas, John P. <John.P.Lucas@nasa.gov>
2024-08-14 10:06:18 -0400

    [nasa/cryptolib#262] Added additional flags to docker run command;

--- support/scripts/env.sh
+++ support/scripts/env.sh
@@ -7,6 +7,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export BASE_DIR=$(cd `dirname $SCRIPT_DIR`/.. && pwd)
-export DFLAGS="docker run --rm -it"
+
+DFLAGS="docker run --rm -it -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $(id -u $(stat -c '%U' $SCRIPT_DIR/env.sh)):$(getent group $(stat -c '%G' $SCRIPT_DIR/env.sh) | cut -d: -f3)"
 
 DBOX="ivvitc/cryptolib:20240814"
