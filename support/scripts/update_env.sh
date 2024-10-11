5b9923ab4827a2be42136e3de4b9031ca8f18775
Donnie-Ice <dri0001@mix.wvu.edu>
2024-10-11 07:00:23 +0000

    [nasa/cryptolib#331] remove sudo from update_env.sh

--- support/scripts/update_env.sh
+++ support/scripts/update_env.sh
@@ -7,6 +7,6 @@
 apt-get install -y lcov libcurl4-openssl-dev libmariadb-dev libmariadb-dev-compat python3
 pip install pycryptodome
 curl -LS https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.50.tar.bz2 -o /tmp/libgpg-error-1.50.tar.bz2 
-tar -xjf /tmp/libgpg-error-1.50.tar.bz2 -C /tmp/ && cd /tmp/libgpg-error-1.50 && sudo ./configure && sudo make install 
+tar -xjf /tmp/libgpg-error-1.50.tar.bz2 -C /tmp/ && cd /tmp/libgpg-error-1.50 && ./configure && make install 
 curl -LS https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.11.0.tar.bz2 -o /tmp/libgcrypt-1.11.0.tar.bz2 
-tar -xjf /tmp/libgcrypt-1.11.0.tar.bz2 -C /tmp/ && cd /tmp/libgcrypt-1.11.0 && sudo ./configure && sudo make install && sudo ldconfig    
+tar -xjf /tmp/libgcrypt-1.11.0.tar.bz2 -C /tmp/ && cd /tmp/libgcrypt-1.11.0 && ./configure && make install && ldconfig    
