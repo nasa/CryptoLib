3c63da1f919c48b913524a6bdeddf1eea77dfe65
Donnie-Ice <dri0001@mix.wvu.edu>
2024-10-24 18:46:42 +0000

    [nasa/cryptolib#156] Removed et_dt_validation and pycryptodome references

--- support/scripts/update_env.sh
+++ support/scripts/update_env.sh
@@ -5,7 +5,6 @@
 #
 
 sudo apt-get install -y lcov libcurl4-openssl-dev libmariadb-dev libmariadb-dev-compat python3
-sudo pip install pycryptodome
 curl -LS https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.50.tar.bz2 -o /tmp/libgpg-error-1.50.tar.bz2 
 tar -xjf /tmp/libgpg-error-1.50.tar.bz2 -C /tmp/ && cd /tmp/libgpg-error-1.50 && sudo ./configure && sudo make install 
 curl -LS https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.11.0.tar.bz2 -o /tmp/libgcrypt-1.11.0.tar.bz2 
