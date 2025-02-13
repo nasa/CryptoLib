cd772d8c75d40a9f655e48194d1b14dc75040157
Donnie-Ice <dri0001@mix.wvu.edu>
2025-02-13 13:27:44 +0000

    [nasa/nos3#483] Uncomment App ID check

--- src/core/crypto.c
+++ src/core/crypto.c
@@ -794,8 +794,9 @@ int32_t Crypto_Process_Extended_Procedure_Pdu(TC_t *tc_sdls_processed_frame, uin
     if (status == CRYPTO_LIB_SUCCESS)
     {
         // Check for specific App ID for EPs - the CryptoLib Apid in this case
-        // if ((tc_sdls_processed_frame->tc_pdu[0] == 0x18) && (tc_sdls_processed_frame->tc_pdu[1] == 0x80))
-        // {
+        if ((tc_sdls_processed_frame->tc_pdu[0] == 0x18) && (tc_sdls_processed_frame->tc_pdu[1] == 0x80))
+        {
+
 #ifdef CRYPTO_EPROC
             // Check validity of SAs used for EP
             if (valid_ep_sa == CRYPTO_TRUE)
@@ -904,10 +905,10 @@ int32_t Crypto_Process_Extended_Procedure_Pdu(TC_t *tc_sdls_processed_frame, uin
             valid_ep_sa = valid_ep_sa; // Suppress build error
             status      = CRYPTO_LIB_SUCCESS;
 #endif // CRYPTO_EPROC
-        // }
+        }
 
         // If not a specific APID, check if using VCIDs for SDLS PDUs with no packet layer
-        if (tc_sdls_processed_frame->tc_header.vcid == TC_SDLS_EP_VCID)
+        else if (tc_sdls_processed_frame->tc_header.vcid == TC_SDLS_EP_VCID)
         {
 #ifdef CRYPTO_EPROC
             // Check validity of SAs used for EP
