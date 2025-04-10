0a43016882c18c096164a8e4e19fd87aeafbc373
rjbrown6 <Robert.J.Brown@nasa.gov>
2025-04-10 11:11:28 -0400

    [nasa/nos3#615] Wrap evn.sh variables/calls in logic to handle MACOS systems.

--- support/scripts/env.sh
+++ support/scripts/env.sh
@@ -5,9 +5,38 @@
 #  source ./env.sh
 #
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-export BASE_DIR=$(cd `dirname $SCRIPT_DIR`/.. && pwd)
+# Helper Functions for conditional stat -c calls
+get_file_user() {
+    if stat --version >/dev/null 2>&1; then
+        stat -c '%U' "$1"
+    else
+        stat -f '%Su' "$1"
+    fi
+}
 
-DFLAGS="docker run --rm -it -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $(id -u $(stat -c '%U' $SCRIPT_DIR/env.sh)):$(getent group $(stat -c '%G' $SCRIPT_DIR/env.sh) | cut -d: -f3)"
+get_file_group() {
+    if stat --version >/dev/null 2>&1; then
+        stat -c '%G' "$1"
+    else
+        stat -f '%Sg' "$1"
+    fi
+}
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export BASE_DIR=$(cd "$(dirname "$SCRIPT_DIR")"/.. && pwd)
+
+file_user=$(get_file_user "$SCRIPT_DIR/env.sh")
+file_group=$(get_file_group "$SCRIPT_DIR/env.sh")
+
+# Conditional mount for passwd/group files (Linux only)
+if [[ "$(uname)" == "Linux" ]]; then
+    PASSWD_MOUNTS="-v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro"
+else
+    PASSWD_MOUNTS=""
+fi
+
+DFLAGS="docker run --rm -it \
+    $PASSWD_MOUNTS \
+    -u $(id -u "$file_user"):$(getent group "$file_group" | cut -d: -f3)"
 
 DBOX="ivvitc/cryptolib:20250108"
