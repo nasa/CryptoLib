a35e1bc72a5b0975dec93167523cd6c00ab1464c
Donnie-Ice <dri0001@mix.wvu.edu>
2025-07-08 13:29:53 -0400

    [nasa/nos3#637] restore dash instead of underscore

--- support/standalone/standalone.h
+++ support/standalone/standalone.h
@@ -47,8 +47,8 @@ extern "C"
 ** Configuration
 */
 #define CRYPTOLIB_HOSTNAME  "cryptolib"
-#define GSW_HOSTNAME        "cosmos"
-#define SC_HOSTNAME         "radio_sim"
+#define GSW_HOSTNAME        "yamcs"
+#define SC_HOSTNAME         "radio-sim"
 #define TC_APPLY_PORT       6010
 #define TC_APPLY_FWD_PORT   8010
 #define TM_PROCESS_PORT     8011
